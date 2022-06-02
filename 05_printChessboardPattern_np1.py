cbcc514810665e1a9a9bb65eb6ec2724c46a791f
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-02 14:03:32 +0530

    added

---
+++ 05_printChessboardPattern_np1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def printChessboardPattern(n):
+    """This function will print 2D array with alternative 0's and 1's like chessboard"""
+
+    result = np.zeros((n,n), dtype=int)
+    for i in range(n):
+        if i%2 == 0:
+            result[i][0] = 1
+
+        for j in range(1,n):
+            if result[i][j-1] == 0:
+                result[i][j]=1
+            print(result[i][j], end=" ")
+        print()
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: ").strip())
+    printChessboardPattern(n)
