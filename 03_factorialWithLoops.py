9ca0fc32778f48a52b74ac276565034acef3a8b7
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-02 13:39:14 +0530

    added sample input cases

--- 03_factorialWithLoops.py
+++ 03_factorialWithLoops.py
@@ -12,3 +12,19 @@ def factorialWithLoops(n):
 if __name__=="__main__":
     n = int(input("Enter the number: "))
     print(f"Factorial of {n} is {factorialWithLoops(n)}")
+
+
+
+# samples:
+
+# Enter the number: 10
+# Factorial of 10 is 3628800
+
+# Enter the number: 0
+# Factorial of 0 is 1
+
+# Enter the number: 1
+# Factorial of 1 is 1
+
+# Enter the number: -10
+# Factorial of -10 is 0
