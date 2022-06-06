b0ab3bd3a64cc4e8d460278ef1a3b262232b1151
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-06 23:47:48 +0530

    added

--- fibonacciCheckFormula.py
+++ fibonacciCheckFormula.py
@@ -1,5 +1,33 @@
 from math import sqrt
 
 # A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square
+def perfectSqrt(n):
+    sq = int(sqrt(n))
+    return sq * sq == n
 
-def fibonacciCheckFormula(n)
+
+def fibonacciCheckFormula(n):
+    if perfectSqrt(5*n*n + 4) or perfectSqrt(5*n*n - 4):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: ").strip())
+    if fibonacciCheckFormula(n):
+        print(f"{n} is a Fibonacci Number!")
+    else:
+        print(f"{n} is not a Fibonacci Number!")
+
+
+# Sample:
+
+# Enter a number: 8
+# 8 is a Fibonacci Number!
+
+# Enter a number: 34
+# 34 is a Fibonacci Number!
+
+# Enter a number: 35
+# 35 is not a Fibonacci Number!
