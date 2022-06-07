157ede53930ab2ca8608660a38ad0e22bbac09ca
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-07 16:11:10 +0530

    added

---
+++ 01_array_programs/03_arrayRotation.py
@@ -0,0 +1,25 @@
+def rotateArray(arr, n, d):
+    result = []
+    for i in range(d,n):
+        result.append(arr[i])
+    
+    for i in range(n-d):
+        result.append(arr[i])
+    
+    return result[:n]
+
+
+if __name__ == "__main__":
+    n = int(input("Enter the length of Array: ").strip())
+    arr = list(map(int, input("Enter the array: ").split()))
+    d = int(input("Enter the displacement: ").strip())
+
+    print(rotateArray(arr, n, d))
+
+
+# Sample:
+
+# Enter the length of Array: 7
+# Enter the array: 1 2 3 4 5 6 7
+# Enter the displacement: 3
+# [4, 5, 6, 7, 1, 2, 3]
