159213e2fd444aa6b2ff4c48dc25626a984dc5e3
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-10 17:22:06 +0530

    minor changes

--- list-programs/11_secoundLargestInList.py
+++ list-programs/11_secoundLargestInList.py
@@ -1,18 +1,18 @@
 def secondLargest(lst):
-    largest = lst[0]
+    largest = lst[0] if lst[0]>lst[1] else lst[1]
     largest2 = lst[0] if lst[0]<lst[1] else lst[1]
     for i in lst:
         if i>largest:
             largest2 = largest
             largest = i
-        elif i>largest2 and i<largest:
+        elif i>largest2 and i != largest:
             largest2 = i
     print(f"Second Largest using for loop: {largest2}")
 
     lst.sort()
     print(f"Secound Largest using sort(): {lst[len(lst)-2]}")
 
-    
+
 if __name__ == "__main__":
     lst = [12, 10, 3, 5, 7, 2, 9, 11]
     print(f"Sample List is {lst}")
