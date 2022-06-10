5704b3d712a643d4032ec5e211d7e3324ea86476
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-10 12:32:28 +0530

    added sample case

--- list-programs/06_reversingList.py
+++ list-programs/06_reversingList.py
@@ -28,6 +28,19 @@ if __name__ == "__main__":
     lst2 = []
     for i in lst:
         lst2.insert(0, i)
+    lst = lst2.copy()
     print(f"Reverse using insert(): {lst2}")
     del lst2
 
+    print(f"Reverse using slicing: {lst}")
+
+
+# Sample:
+
+# Sample List: [2, 5, 10, 12, 13, 15, 20, 16, 25, 1, 30]
+# Reverse using for loop: [30, 1, 25, 16, 20, 15, 13, 12, 10, 5, 2]
+# Reverse using while loop: [2, 5, 10, 12, 13, 15, 20, 16, 25, 1, 30]
+# Reverse using reverse(): [30, 1, 25, 16, 20, 15, 13, 12, 10, 5, 2]
+# Reverse using reversed(): [2, 5, 10, 12, 13, 15, 20, 16, 25, 1, 30]
+# Reverse using insert(): [30, 1, 25, 16, 20, 15, 13, 12, 10, 5, 2]
+# Reverse using slicing: [30, 1, 25, 16, 20, 15, 13, 12, 10, 5, 2]
