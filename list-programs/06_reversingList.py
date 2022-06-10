b71d99dbcf200bba1213d3cf251eb85e4c81e451
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-10 12:13:20 +0530

    added alternative

--- list-programs/06_reversingList.py
+++ list-programs/06_reversingList.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
         # lst[i] = lst[adjI]
         # lst[adjI] = temp
         lst[i], lst[adjI] = lst[adjI], lst[i]
-        
     print(f"Reverse using for loop: {lst}")
 
     lst = [2,5,10,12,13,15,20,16,25,1,30]
     start = 0
-    end = n
+    end = n-1
     while(start<end):
+        lst[start], lst[end] = lst[end], lst[start]
+        start = start+1
+        end = end-1
+    print(f"Reverse using while loop: {lst}")
