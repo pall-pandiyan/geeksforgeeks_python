514d2e6d4333bc159fb5155be2f17b7ed2a9ca05
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-10 13:07:55 +0530

    added alternative and a sample case

--- list-programs/07_sumOfList.py
+++ list-programs/07_sumOfList.py
@@ -6,27 +6,38 @@ def sumOfListRecursion(lst, size):
 
 
 def sumOfList(lst):
-    sum = 0
+    total = 0
     for i in lst:
-        sum = sum+i
-    print(f"Sum using for loop: {sum}")
+        total = total+i
+    print(f"Sum using for loop: {total}")
 
     start = 0
     end = len(lst)-1
-    sum = 0
+    total = 0
     while(start <= end):
         if start != end:
-            sum = sum + lst[start] + lst[end]
+            total = total + lst[start] + lst[end]
         else:
-            sum = sum + lst[start]
+            total = total + lst[start]
         start = start+1
         end = end-1
-    print(f"Sum using while loop: {sum}")
+    print(f"Sum using while loop: {total}")
 
     print(f"Sum using Recursion: {sumOfListRecursion(lst, len(lst))}")
+
+    print(f"Sum using sum(): {sum(lst)}")
 
 
 if __name__ == "__main__":
     lst = [10, 12, 15, 16, 17, 23, 1, 2, 5, 25, 4]
     print(f"Sample List: {lst}")
     sumOfList(lst)
+
+
+# Sample:
+
+# Sample List: [10, 12, 15, 16, 17, 23, 1, 2, 5, 25, 4]
+# Sum using for loop: 130
+# Sum using while loop: 130
+# Sum using Recursion: 130
+# Sum using sum(): 130
