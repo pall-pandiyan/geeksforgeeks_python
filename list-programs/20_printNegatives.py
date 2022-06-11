8e4044178fe84b7deaa99f3855e3ad3e3649cb4a
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-11 17:25:26 +0530

    added a alternative

--- list-programs/20_printNegatives.py
+++ list-programs/20_printNegatives.py
@@ -9,6 +9,12 @@ def printNegatives(start, end):
         print(i, end=" ")
     print()
 
+    print(f"negatives in range using while loop:", end=" ")
+    i = start
+    while i!=end:
+        print(i, end=" ")
+        i = i+1
+    print()
 
 if __name__ == "__main__":
     start = int(input("Enter the start range: ").strip())
@@ -18,7 +24,8 @@ if __name__ == "__main__":
 
 # sample:
 
-# Enter the start range: -3
-# Enter the end range: 5
-# negatives in range using range(): [-3, -2, -1]
-# negatives in range using for loop: -3 -2 -1
+# Enter the start range: -18
+# Enter the end range: 1
+# negatives in range using range(): [-18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+# negatives in range using for loop: -18 -17 -16 -15 -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 
+# negatives in range using while loop: -18 -17 -16 -15 -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
