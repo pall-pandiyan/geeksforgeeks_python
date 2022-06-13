3825c88f4222b592733e99996fcc5b068e22dd1b
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-13 23:03:57 +0530

    added a alternative method

--- list-programs/26_printDuplicates.py
+++ list-programs/26_printDuplicates.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def print_duplicates(lst):
     result = []
     l = set(lst)
@@ -5,6 +7,17 @@ def print_duplicates(lst):
         if lst.count(ele)>0:
             result.append(ele)
     print(f"duplicates using count(): {result}")
+
+    d = dict(Counter(lst))
+    # print(type(dict(d)))
+
+    result = []
+    for k in d:
+        if d[k]>0:
+            result.append(k)
+    print(f"duplicates using Counter: {result}")
+
+
 
 if __name__ == "__main__":
     lst = [1,1,20,10,30,3,10,5]
@@ -16,3 +29,4 @@ if __name__ == "__main__":
 
 # Sample List: [1, 1, 20, 10, 30, 3, 10, 5]
 # duplicates using count(): [1, 3, 5, 10, 20, 30]
+# duplicates using Counter: [1, 20, 10, 30, 3, 5
