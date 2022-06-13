8d4b05bd68215a5addfb1e3770a8b0484a81fd58
pall-pandiyan <pallpandiyan@gmail.com>
2022-06-13 22:05:17 +0530

    added alternatives methods

--- list-programs/24_countElements.py
+++ list-programs/24_countElements.py
@@ -1,9 +1,28 @@
+from collections import Counter
+
 def count_elements(lst):
     l = set(lst)
-    print("count element using count(): ")
+    # print("count element using count(): ")
+    result = {}
     for ele in l:
-        print(f"{ele}x{lst.count(ele)}")
-    print()
+        # print(f"{ele}x{lst.count(ele)}")
+        result[ele] = lst.count(ele)
+    print(f"count using count(): {result}")
+
+    result = {}
+    for k in l:
+        count = 0
+        for v in lst:
+            if v == k:
+                count = count+1
+        result[k] = count
+    print(f"count using for loop: {result}")
+
+    c = Counter(lst)
+    result = {}
+    for k in l:
+        result[k] = c[k]
+    print(f"count using Counter: {result}")
 
 
 if __name__ == "__main__":
@@ -15,9 +34,6 @@ if __name__ == "__main__":
 # Sample:
 
 # Sample List: [1, 5, 9, 6, 3, 1, 5, 3, 1, 9]
-# count element using count(): 
-# 1x3
-# 3x2
-# 5x2
-# 6x1
-# 9x2
+# count using count(): {1: 3, 3: 2, 5: 2, 6: 1, 9: 2}
+# count using for loop: {1: 3, 3: 2, 5: 2, 6: 1, 9: 2}
+# count using Counter: {1: 3, 3: 2, 5: 2, 6: 1, 9: 2}
